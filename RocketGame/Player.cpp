db013eb0830bcc4e1a958be9af62d6c49bffcf1d
Zeke <hnta3574@gmail.com>
2018-11-25 23:12:03 +0900

    fix player move func

--- RocketGame/Player.cpp
+++ RocketGame/Player.cpp
@@ -14,6 +14,11 @@ Player::~Player()
 bool Player::Start() {
 	m_model = NewGO<SkinModelRender>(0, "octane");
 	m_model->Init(L"Assets/modelData/Octane.cmo", nullptr, 0, enFbxUpAxisY);
+	CQuaternion sRot = m_rot;
+	sRot.SetRotationDeg(CVector3::AxisY(), 45.f + 180.f);
+	m_model->SetRotation(sRot);
+	m_forward = MainCamera().GetForward();
+	sRot.Multiply(m_forward);
 	return true;
 }
 
@@ -26,36 +31,57 @@ void Player::Update() {
 	Rotation();
 }
 
+//feature move. パッドで回すのは加速ベクトルだけにして、そのベクトルのrotをモデルに適用するだけにする
 void Player::Move() {
+	char message[256];
+
 	float MOVE_SPEED = 50.0f;
 	static float MOVE_SPEED_JUMP = 30.0f;
+	float minSpeed = 1.3f;
 	float x = Pad(0).GetLStickXF();
 	float y = Pad(0).GetLStickYF();
 	float r = Pad(0).GetRTrigger();
 	float l = Pad(0).GetLTrigger();
-	//回すよ
-	CQuaternion qRot;
-	qRot.SetRotationDeg(CVector3::AxisY(), 2.0f * x);
-	m_rot.Multiply(qRot);
-	//m_model->SetRotation(m_rot);
-	//慣性
-	accVec = CVector3::One();
+	//アクセルが踏まれたら踏まれた角度によって加速ベクトルの大きさを変化させる
+	CVector3 accVec = CVector3::One();
 	accVec *= r * MOVE_SPEED * GameTime().GetFrameDeltaTime();
 	accVec.y = 0.f;
+	//スティック（ハンドル）で加速ベクトルの向きを決めていく
+	CQuaternion qRot;
+	if (fabsf(m_moveSpeed.x) > minSpeed || fabsf(m_moveSpeed.z) > minSpeed) {
+		qRot.SetRotationDeg(CVector3::AxisY(), 1.4f * x);
+	}
+	else {
+		//回れるほどの速度が出てないよ
+		qRot = CQuaternion::Identity();
+	}
+	m_rot.Multiply(qRot);
 	m_rot.Multiply(accVec);
+	//車の前方向を保持しておく
+	qRot.Multiply(m_forward);
+	//前方向と加速ベクトルを加算する
+	CVector3 toPos;
+	toPos = accVec;
+	//摩擦を計算する
 	CVector3 friction = m_moveSpeed;
-	friction *= -1.5f;
-	m_moveSpeed.x += friction.x * GameTime().GetFrameDeltaTime();
-	m_moveSpeed.z += friction.z * GameTime().GetFrameDeltaTime();
-
-	//加速度を加える。
-	m_moveSpeed += accVec;
+	friction *= m_frictionParam;
+	//加速ベクトルのxz成分に摩擦を加算する
+	m_moveSpeed.x +=  friction.x * GameTime().GetFrameDeltaTime();
+	m_moveSpeed.z +=  friction.z * GameTime().GetFrameDeltaTime();
+	//移動ベクトルに加速ベクトルを加算する
+	m_moveSpeed += toPos;
+	//モデルを加速ベクトルの向きに向かせる(Rotationでやるわ)
+	
+	//BoostTest
+	if (Pad(0).IsPress(enButtonB)) {
+		m_moveSpeed += m_forward * m_boostParam;
+	}
+	//モデルの座標に移動ベクトルを加算する
+	qRot.Multiply(m_moveSpeed);
 	m_pos += m_moveSpeed;
-	m_model->SetPosition(m_pos);
-
-	char message[256];
-	sprintf_s(message, "x...%f z...%f\n", fabsf(m_moveSpeed.x), fabsf(m_moveSpeed.z));
+	sprintf_s(message, "Speed x...%f z...%f\n", m_moveSpeed.x, m_moveSpeed.z);
 	OutputDebugStringA(message);
+	m_model->SetPosition(m_pos);
 }
 
 void Player::Rotation() {
@@ -63,11 +89,8 @@ void Player::Rotation() {
 		&& fabsf(m_moveSpeed.z) < 0.001f) {
 		return;
 	}
-	float angle = atan2(m_moveSpeed.x, m_moveSpeed.z);
-	CQuaternion nRot;
-	nRot.SetRotation(CVector3::AxisY(), angle);
-	CQuaternion aRot;
-	aRot.SetRotationDeg(CVector3::AxisY(),180.f);
-	nRot.Multiply(aRot);
+	CQuaternion nRot = m_rot;
+	nRot.SetRotationDeg(CVector3::AxisY(), 45.f + 180.f);
+	nRot.Multiply(m_rot);
 	m_model->SetRotation(nRot);
 }
