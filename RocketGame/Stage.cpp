e9dc7428002fa5efb1d06f90c8e409995b9e2b60
Zeke <hnta3574@gmail.com>
2018-11-25 23:56:53 +0900

    add stage

--- RocketGame/Stage.cpp
+++ RocketGame/Stage.cpp
@@ -13,7 +13,7 @@ Stage::~Stage()
 
 bool Stage::Start() {
 	m_model = NewGO<SkinModelRender>(0, "Stage");
-	m_model->Init(L"Assets/modelData/Stage.cmo", nullptr, 0, enFbxUpAxisY);
+	m_model->Init(L"Assets/modelData/Tofu.cmo", nullptr, 0, enFbxUpAxisZ);
 	m_model->SetPosition(m_pos);
 	m_rot.SetRotationDeg(CVector3::AxisY(), 90.f);
 	m_model->SetRotation(m_rot);
